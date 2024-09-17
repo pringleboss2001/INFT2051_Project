c8adff7954fd22e7204ccacf4a7dd4b228c9a00d
pringleboss2001 <c3355958@uon.edu.au>
2024-09-17 14:54:41 +1000

    No changes

--- INFT2051_Project/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/INFT2051_Project.AssemblyInfo.cs
+++ INFT2051_Project/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/INFT2051_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INFT2051_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bc0f92569781825245a132d6cea1f60b310c07a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+12e30eb8b87112019ffb0abff95bbe48bbe3d39f")]
 [assembly: System.Reflection.AssemblyProductAttribute("INFT2051_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INFT2051_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
