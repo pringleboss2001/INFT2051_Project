bc0f92569781825245a132d6cea1f60b310c07a7
pringleboss2001 <c3355958@uon.edu.au>
2024-09-17 11:27:37 +1000

    minor

--- INFT2051_Project/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/INFT2051_Project.AssemblyInfo.cs
+++ INFT2051_Project/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/INFT2051_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INFT2051_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+69443a6193285b7355aa00d31ceeaa5426f29878")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9c52d5e33d30af2a15b7d581581a23d6dbafee0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("INFT2051_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INFT2051_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
