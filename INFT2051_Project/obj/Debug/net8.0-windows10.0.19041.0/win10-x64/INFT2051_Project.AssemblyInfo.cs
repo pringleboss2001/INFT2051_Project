9c52d5e33d30af2a15b7d581581a23d6dbafee0a
pringleboss2001 <c3355958@uon.edu.au>
2024-09-17 10:49:08 +1000

    cleaning

--- INFT2051_Project/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/INFT2051_Project.AssemblyInfo.cs
+++ INFT2051_Project/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/INFT2051_Project.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INFT2051_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ee096d1e9b936f0180d3835fdcfa4870f1862912")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+69443a6193285b7355aa00d31ceeaa5426f29878")]
 [assembly: System.Reflection.AssemblyProductAttribute("INFT2051_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INFT2051_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
